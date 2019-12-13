48c6e64a73b5c7368a46a632bd232fc6854f3fc0
nsinghsingh <nihal.singh@csbe.ch>
2019-12-13 20:51:32 +0100

    Updated menu

---
+++ Japanese Ripoff/Assets/Scripts/Tooltip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Tooltip : MonoBehaviour
+{
+    public GameObject icon;
+    public string help = "";
+    public Font font;
+    private GameObject tooltip;
+
+
+    public void makeBubble()
+    {
+        Vector2 location = icon.transform.position;
+        location.x += 400;
+        tooltip = new GameObject();
+        tooltip.transform.parent = icon.transform;
+        tooltip.transform.position = location;
+        tooltip.AddComponent<Text>();
+        Text message = tooltip.GetComponent<Text>();
+        message.text = help;
+        message.fontSize = 32;
+        message.font = font;
+        message.color = new Color(0, 0, 0);
+        message.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 400);
+    }
+
+    public void destroyBubble()
+    {
+        Destroy(tooltip);
+    }
+}
